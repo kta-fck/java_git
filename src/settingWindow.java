2e7fdcf76244a7be7d50e59a5f8dc5172e806a1a
keita_tap <kta.fck@gmail.com>
2014-09-08 15:22:29 +0900

    settingWindow#getMasterArrayList 引数はFile型
    マスタファイルを読み込んでListへ代入するメソッド作成。
    戻り値はオブジェクト型のList

--- src/settingWindow.java
+++ src/settingWindow.java
@@ -1,23 +1,38 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 
 public class settingWindow {
 
 
-
 	// クラスフィールド
-
+	String readString = "";
+	ArrayList<Object> mstrList;
 	String fileStr = "";
 	File filePath;
 
-	public settingWindow() {
+	public settingWindow() {}
 
 
+	public ArrayList<Object> getMasterArrayList (File file) {
+
+
+	try {
+		BufferedReader breader = new BufferedReader(new FileReader(file));
+
+		while(breader.ready()) {
+			readString = breader.toString();
+			mstrList.add(readString);
+		}
+	} catch (IOException e )  {
+		System.out.println("ファイル読み込みエラー");
 	}
-
-
-
+		return mstrList;
 	}
-
 }
+
+
