405ab5b7674b2d2e91d584f3f9df2610d2f637c5
kta-fck <kta.fck@gmail.com>
2014-09-11 11:33:48 +0900

    change style; button and checkbox -> tabbed pane.

--- src/settingWindow.java
+++ src/settingWindow.java
@@ -11,6 +11,10 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
 
 
 
@@ -25,14 +29,18 @@ public class settingWindow extends JFrame {
 	File filePath;
 
 	JButton settingButton = new JButton("登録と編集");
-	JButton masterButton = new JButton("マスタ^");
+	JButton masterButton = new JButton("マスタ");
 
 	JLabel stnBtnLabel = new JLabel("ユーザーの登録と編集");
 	JLabel mstrBtnLabel = new JLabel("ユーザのマスタファイルの選択");
 
 	JCheckBox cnfrmbox = new JCheckBox("接続前に確認を行う");
 
+	// tabbed pane
+	JPanel regAndEditPanel = new JPanel();
+	JPanel masterAndSettingPanel = new JPanel();
 
+	JTabbedPane regAndEditPane = new JTabbedPane();
 
 	GridBagLayout stnGbl = new GridBagLayout();
 	GridBagConstraints stnGbc = new GridBagConstraints();
@@ -41,39 +49,52 @@ public class settingWindow extends JFrame {
 
 	public settingWindow () {
 
-		setSize(400, 300);
-		setTitle("設定画面");
-		setLayout(stnGbl);
+		setBounds(400, 130, 400, 300);
 
+		setTitle("設定画面");
+		regAndEditPanel.setLayout(stnGbl);
+		masterAndSettingPanel.setLayout(stnGbl);
+
+		// -------------------------------------------------------------------------------------
+		// 登録と編集タブ
 		stnGbc.gridx = 0; stnGbc.gridy = 0 ;stnGbc.gridwidth = 1;
 		stnGbc.weightx = 1.0; stnGbc.weighty = 1.0; stnGbc.fill = GridBagConstraints.HORIZONTAL;
 		stnGbc.insets = new Insets(1, 1, 1, 1);
 		stnGbl.setConstraints(settingButton, stnGbc);
-		getContentPane().add(settingButton);
+		regAndEditPanel.add(settingButton);
 
 		stnGbc.gridx = 0; stnGbc.gridy = 1 ;stnGbc.gridwidth = 1;
 		stnGbc.weightx = 1.0; stnGbc.weighty = 1.0; stnGbc.fill = GridBagConstraints.HORIZONTAL;
 		stnGbc.insets = new Insets(1, 1, 1, 1);
 		stnGbl.setConstraints(masterButton, stnGbc);
-		getContentPane().add(masterButton);
+		regAndEditPanel.add(masterButton);
 
 		stnGbc.gridx = 1; stnGbc.gridy = 0 ;stnGbc.gridwidth = 1;
 		stnGbc.weightx = 5.0; stnGbc.weighty = 1.0; stnGbc.fill = GridBagConstraints.BOTH;
 		stnGbc.insets = new Insets(1, 1, 1, 1);
 		stnGbl.setConstraints(stnBtnLabel, stnGbc);
-		getContentPane().add(stnBtnLabel);
+		regAndEditPanel.add(stnBtnLabel);
 
 		stnGbc.gridx = 1; stnGbc.gridy = 1 ;stnGbc.gridwidth = 1;
 		stnGbc.weightx = 5.0; stnGbc.weighty = 1.0; stnGbc.fill = GridBagConstraints.BOTH;
 		stnGbc.insets = new Insets(1, 1, 1, 1);
 		stnGbl.setConstraints(mstrBtnLabel, stnGbc);
-		getContentPane().add(mstrBtnLabel);
+		regAndEditPanel.add(mstrBtnLabel);
 
+
+		// ---------------------------------------------------------------------------------
+		// マスタタブ
 		stnGbc.gridx = 0; stnGbc.gridy = 2 ;stnGbc.gridwidth = 2;
 		stnGbc.weightx = 1.0; stnGbc.weighty = 1.0; stnGbc.fill = GridBagConstraints.BOTH;
 		stnGbc.insets = new Insets(1, 1, 1, 1);
 		stnGbl.setConstraints(cnfrmbox, stnGbc);
-		getContentPane().add(cnfrmbox);
+		masterAndSettingPanel.add(cnfrmbox);
+
+
+		regAndEditPane.add(regAndEditPanel, "登録と編集");
+		regAndEditPane.add(masterAndSettingPanel, "マスタと設定");
+
+		getContentPane().add(regAndEditPane);
 
 
 	}
